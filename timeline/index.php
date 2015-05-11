827f546170023cc4f9f1eaaecf6abf16a2891749
DASungta <z.y.c.7777@163.com>
2015-05-11 17:39:10 +0800

    version1.0
    时间轴初始版本

---
+++ timeline/index.php
@@ -0,0 +1,13 @@
+<?php
+
+define('APP_DEBUG',True);
+
+// 绑定Admin模块到当前入口文件
+// define('BIND_MODULE','Admin');
+
+// 定义应用目录
+define('APP_PATH','./Application/');
+
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+?>
